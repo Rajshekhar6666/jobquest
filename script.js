b96b4b0d7698014aa950d07663f33ce141b31447
Rajshekhar6666 <rajshekhar56579@gmail.com>
2024-12-25 21:06:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const jobs = [
+    { title: "Software Engineer", category: "Software", location: "New York", company: "Tech Corp" },
+    { title: "Marketing Manager", category: "Marketing", location: "San Francisco", company: "Creative Agency" },
+    { title: "UI/UX Designer", category: "Design", location: "Remote", company: "Design Studio" }
+  ];
+  
